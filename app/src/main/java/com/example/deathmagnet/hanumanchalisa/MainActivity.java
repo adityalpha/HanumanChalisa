8a3ba090bee507cd316d2d2ab40307118b71176d
adityalpha <adityalpha@gmail.com>
2018-06-25 06:26:30 -0700

    Git test

--- app/src/main/java/com/example/deathmagnet/hanumanchalisa/MainActivity.java
+++ app/src/main/java/com/example/deathmagnet/hanumanchalisa/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.deathmagnet.hanumanchalisa;
-
+//git test
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
